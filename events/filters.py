be31641fb5f508d2ccc75b6047d5f9bf5e646dd6
Oluwaseyi <seyistry@gmail.com>
2024-10-13 12:25:26 +0100

    add filter

--- events/filters.py
+++ events/filters.py
@@ -4,8 +4,8 @@ from .models import Event
 class EventFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(field_name="title", lookup_expr='icontains')  # Case-insensitive search
     location = django_filters.CharFilter(field_name="location", lookup_expr='icontains')
-    start_date = django_filters.DateFilter(field_name="date_time", lookup_expr='gte')  # Start of date range
-    end_date = django_filters.DateFilter(field_name="date_time", lookup_expr='lte')    # End of date range
+    start_date = django_filters.DateFilter(field_name="date_time", lookup_expr='gte')  # Start of date range example 2024-01-01
+    end_date = django_filters.DateFilter(field_name="date_time", lookup_expr='lte')    # End of date range example 2024-12-31
 
     class Meta:
         model = Event
