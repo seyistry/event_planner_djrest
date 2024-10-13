149fc88ba4868687e594b535a503c7f6bc0f08ee
Oluwaseyi <seyistry@gmail.com>
2024-10-13 12:33:17 +0100

    remove restriction for event details

--- events/views.py
+++ events/views.py
@@ -1,11 +1,16 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics, permissions, filters, pagination
+from rest_framework import generics, permissions, filters, pagination, status
 from rest_framework.response import Response
-from .serializers import EventSerializer, UserSerializer
+from .serializers import (
+    EventSerializer,
+    UserSerializer,
+    EventRegistrationSerializer
+)
 from .permissions import IsOwnerOrReadOnly
 from .models import Event
 from django_filters.rest_framework import DjangoFilterBackend
 from .filters import EventFilter
+from django.utils import timezone
 
 User = get_user_model()
 
@@ -25,60 +30,87 @@ class EventDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = EventSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
 
-    # Enforce that users can only edit or delete their own events
-    def get_queryset(self):
-        return Event.objects.filter(organizer=self.request.user)
-
 
 class EventRegisterView(generics.GenericAPIView):
     queryset = Event.objects.all()
     permission_classes = [permissions.IsAuthenticated]
+    serializer_class = EventRegistrationSerializer
+
+    def get_object(self):
+        """Retrieve the event instance."""
+        try:
+            return Event.objects.get(pk=self.kwargs['pk'])
+        except Event.DoesNotExist:
+            return Response({"detail": "Event not found."}, status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk):
+        """Allow a user to register for an event."""
+        event = self.get_object()
+        if isinstance(event, Response):  # Check if the event retrieval returned a Response
+            return event
+
+        # Check if the user is already attending
+        if request.user in event.attendees.all():
+            return Response({"detail": "You are already registered for this event."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Check if the user is already on the waitlist
+        if request.user in event.waitlist.all():
+            return Response({"detail": "You are already on the waitlist for this event."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # If the event is not full
+        if not event.is_full():
+            return Response({"detail": event.title, "capacity_left": event.capacity - event.attendees.count()}, status=status.HTTP_200_OK)
+
+        # If the event is full
+        return Response({"detail": "The event is full"}, status=status.HTTP_200_OK)
 
     def post(self, request, pk):
         """Allow a user to register for an event."""
         event = self.get_object()
+        if isinstance(event, Response):  # Check if the event retrieval returned a Response
+            return event
 
-        # Check if the user is already attending or on the waitlist
+        # Check if the user is already attending
         if request.user in event.attendees.all():
-            return Response({"detail": "You are already registered for this event."}.HTTP_400_BAD_REQUEST)
+            return Response({"detail": "You are already registered for this event."}, status=status.HTTP_400_BAD_REQUEST)
 
+        # Check if the user is already on the waitlist
         if request.user in event.waitlist.all():
-            return Response({"detail": "You are already on the waitlist for this event."}.HTTP_400_BAD_REQUEST)
+            return Response({"detail": "You are already on the waitlist for this event."}, status=status.HTTP_400_BAD_REQUEST)
 
         # If the event is not full, add the user as an attendee
         if not event.is_full():
             event.attendees.add(request.user)
-            return Response({"detail": "You have successfully registered for the event."}.HTTP_200_OK)
+            return Response({"detail": "You have successfully registered for the event."}, status=status.HTTP_200_OK)
+
+        # If the event is full
+        return Response({"detail": "The event is full"}, status=status.HTTP_200_OK)
+
+
+class JoinWaitListView(generics.GenericAPIView):
+    queryset = Event.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = EventRegistrationSerializer
+
+    def post(self, request, pk):
+        """Allow a user to register for an event."""
+        event = self.get_object()
+        # Check if the user is already attending
+        if request.user in event.attendees.all():
+            return Response({"detail": "You are already registered for this event."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Check if the user is already on the waitlist
+        if request.user in event.waitlist.all():
+            return Response({"detail": "You are already on the waitlist for this event."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # If the event is not full, add the user as an attendee
+        if not event.is_full():
+            event.attendees.add(request.user)
+            return Response({"detail": "You have successfully registered for the event."}, status=status.HTTP_200_OK)
 
         # If the event is full, add the user to the waitlist
         event.waitlist.add(request.user)
         return Response({"detail": "The event is full. You have been added to the waitlist."}, status=status.HTTP_200_OK)
-
-# View for user registration
-
-
-class ManageWaitlistView(generics.GenericAPIView):
-    queryset = Event.objects.all()
-    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
-
-    def post(self, request, pk):
-        """Move users from the waitlist to attendees if there is available capacity."""
-        event = self.get_object()
-
-        # Only allow the event organizer to manage the waitlist
-        if event.organizer != request.user:
-            return Response({"detail": "You do not have permission to manage the waitlist."}, status=status.HTTP_403_FORBIDDEN)
-
-        # Move users from waitlist to attendees while there are open spots
-        if not event.is_full():
-            waitlisted_users = event.waitlist.all(
-            )[:event.capacity - event.attendees.count()]
-            for user in waitlisted_users:
-                event.attendees.add(user)
-                event.waitlist.remove(user)
-            return Response({"detail": "Users moved from the waitlist to attendees."}, status=status.HTTP_200_OK)
-
-        return Response({"detail": "No open spots available."}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserCreateView(generics.CreateAPIView):
@@ -111,10 +143,11 @@ class UpcomingEventListView(generics.ListAPIView):
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     filterset_class = EventFilter
     ordering_fields = ['date_time']  # Allows ordering by event date
+    ordering = ['date_time']  # Default ordering
     pagination_class = UpcomingEventListPagination  # Use custom pagination
 
     # Override the queryset to only include upcoming events
     def get_queryset(self):
         queryset = super().get_queryset()
         # Filter out events that have already occurred
-        return queryset.filter(date_time__gte=now())
+        return queryset.filter(date_time__gte=timezone.now())
